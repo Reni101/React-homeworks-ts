07bd41303066612896f176baacf508e02dd1bcac
Maxim Dmitriev <Renissance@tut.by>
2022-05-30 19:43:35 +0300

    Updates

---
+++ precache-manifest.4f471010af41b11e071628eed61477b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559d9d90b0c7ec5b85919e114988a777",
+    "url": "/React-homeworks-ts/index.html"
+  },
+  {
+    "revision": "e294a982d71851891c8a",
+    "url": "/React-homeworks-ts/static/css/main.a5ff3915.chunk.css"
+  },
+  {
+    "revision": "eeced25ea65e9a01a18e",
+    "url": "/React-homeworks-ts/static/js/2.492eb9e5.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/React-homeworks-ts/static/js/2.492eb9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e294a982d71851891c8a",
+    "url": "/React-homeworks-ts/static/js/main.a2d96b8d.chunk.js"
+  },
+  {
+    "revision": "29fd92734d617efc4b58",
+    "url": "/React-homeworks-ts/static/js/runtime-main.eb2a6a63.js"
+  }
+]);

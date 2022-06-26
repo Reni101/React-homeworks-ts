5c3e9453f9da1ba55ff83b5e7c2aabeb69684222
Maxim Dmitriev <Renissance@tut.by>
2022-06-26 16:37:43 +0300

    HomeWork -9

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
         <div className={"HeaderBox"}>
             <NavLink to={PATH.PRE_JUNIOR} className={"Link"}> PreJunior</NavLink>
             <NavLink to={PATH.Junior} className={"Link"}> Junior </NavLink>
-            <NavLink to={PATH.JuniorPlus} className={"Link"}> Junior </NavLink>
+            <NavLink to={PATH.JuniorPlus} className={"Link"}> Junior+ </NavLink>
             <span className={"Navigation"}> Navigation </span>
                 {/*всплывающие окноно я подсмотрел*/}
 

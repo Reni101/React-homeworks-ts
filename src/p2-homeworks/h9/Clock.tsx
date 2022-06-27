3315bb5316f60cb797a6821250e97877053d04f8
Maxim Dmitriev <Renissance@tut.by>
2022-06-27 09:12:14 +0300

    HomeWork -9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -7,7 +7,7 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        clearInterval(timerId)
+        clearInterval(timerId) //подсмотрел
     }
     const start = () => {
         stop()
